51e97ce8e709ca14129efff1294d61a6bee574d0
MariyaEfronY <mariyaefrony@gmail.com>
2026-02-16 15:04:26 +0530

    contact added in the navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,6 +30,7 @@ export default function Navbar() {
         { name: 'Leadership', href: '#leadership' },
         { name: 'Philosophy', href: '#philosophy' },
         { name: 'Services', href: '#services' },
+        { name: 'Contact', href: '#contact' },
     ];
 
     return (
